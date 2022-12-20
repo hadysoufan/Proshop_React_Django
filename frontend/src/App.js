2d186f584e3509c91f8d8d307b0e1514ebcfef3c
hadysoufan <hadisoufan74@gmail.com>
2022-12-20 19:36:11 +0200

    adding route to the cart screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header.components';
 import Home from './screens/Home/Home.screen';
 import Product from './screens/Product/Product.screen';
 import { Container } from 'react-bootstrap';
+import CartScreen from './screens/Cart/Cart.screen';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -13,8 +14,9 @@ function App() {
       <main className="py-3">
         <Container>
           <Routes>
-            <Route path="/" element={<Home />} exact/>
+            <Route path="/" element={<Home />} exact />
             <Route path="/product/:id" element={<Product />} />
+            <Route path="/cart/:id?" element={<CartScreen />} />
           </Routes>
         </Container>
       </main>
